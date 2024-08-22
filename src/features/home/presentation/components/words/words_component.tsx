d5bb9bd909a168a0170e423cf7c070f05990e188
Carlos Henrique <carloshenriquend.santos@gmail.com>
2024-08-22 12:25:37 -0300

    Adiciona tratamento para lista vazia no componente de palavras

--- src/features/home/presentation/components/words/words_component.tsx
+++ src/features/home/presentation/components/words/words_component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {FlatList, Pressable, Text, View} from 'react-native';
 import WordEntity from '../../../domain/entities/word';
 import WordComponent from '../word/word_component';
+import WordsComponentStyle from './words_component_styles';
 
 interface WordsComponentProps {
   words: WordEntity[];
@@ -10,8 +11,19 @@ interface WordsComponentProps {
 const WordsComponent: React.FC<WordsComponentProps> = (
   props: WordsComponentProps,
 ) => {
+  if (props.words.length == 0) {
+    return (
+      <View style={WordsComponentStyle.container}>
+        <Text style={WordsComponentStyle.emptyText}>Você não tem nenhuma</Text>
+        <Text style={WordsComponentStyle.emptyText}>palavra cadastrada 🥺</Text>
+      </View>
+    );
+  }
+
   return (
     <>
+      <Text style={WordsComponentStyle.hasItemText}>PALAVRAS: </Text>
+      <View style={{height: 10}} />
       <FlatList
         data={props.words}
         renderItem={word => (
--- src/features/home/presentation/components/words/words_component_styles.ts
+++ src/features/home/presentation/components/words/words_component_styles.ts
@@ -1,5 +1,20 @@
 import {StyleSheet} from 'react-native';
 
-const WordsComponentStyle = StyleSheet.create({});
+const WordsComponentStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  emptyText: {
+    color: 'white',
+    fontSize: 15,
+  },
+  hasItemText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+});
 
 export default WordsComponentStyle;
