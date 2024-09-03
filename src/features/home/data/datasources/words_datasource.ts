673c71ea3d0465671811156e24ec2c528c4e1054
Carlos Henrique <carloshenriquend.santos@gmail.com>
2024-09-02 21:55:54 -0300

    Cria primeiras queries para salvar e recuperar palavras

---
+++ src/features/home/data/datasources/words_datasource.ts
@@ -0,0 +1,98 @@
+import {SQLiteDatabase} from 'react-native-sqlite-storage';
+import WordEntity from '../../domain/entities/word';
+
+interface IWordsDatasource {
+  init(): Promise<void>;
+  getWords(): Promise<WordEntity[]>;
+  saveWord(word: WordEntity): Promise<void>;
+}
+
+class WordsDatasourceImpl implements IWordsDatasource {
+  db: SQLiteDatabase;
+
+  wordsTableName: string = 'words';
+  wordExamplesTableName: string = 'word_examples';
+
+  constructor(db: SQLiteDatabase) {
+    this.db = db;
+  }
+
+  async init() {
+    const wordsTableQuery = `CREATE TABLE IF NOT EXISTS ${this.wordsTableName}(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      status TEXT NOT NULL,
+      word TEXT NOT NULL,
+      explanation TEXT
+    );`;
+
+    const examplesTableQuery = `CREATE TABLE IF NOT EXISTS ${this.wordExamplesTableName}(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      word_id INTEGER NOT NULL,
+      phrase TEXT NOT NULL,
+      FOREIGN KEY (word_id) REFERENCES ${this.wordsTableName} ON DELETE CASCADE
+    );`;
+
+    await this.db.executeSql(wordsTableQuery);
+    await this.db.executeSql(examplesTableQuery);
+  }
+
+  async getWords(): Promise<WordEntity[]> {
+    try {
+      const words: WordEntity[] = [];
+
+      const results = await this.db.executeSql(
+        `SELECT * FROM ${this.wordsTableName} ORDER BY word`,
+      );
+
+      results.forEach(result => {
+        for (let index = 0; index < result.rows.length; index++) {
+          words.push(result.rows.item(index));
+        }
+      });
+
+      return words;
+    } catch (error) {
+      console.error(error);
+      throw Error('Falha ao recuperar palavras');
+    }
+  }
+
+  async saveWord(word: WordEntity) {
+    try {
+      const saveWordQuery = `INSERT INTO ${this.wordsTableName}(
+        status,
+        word, 
+        explanation
+      ) VALUES (
+        '${word.status}',
+        '${word.word}',
+        '${word.explanation}'
+      );`;
+
+      await this.db.executeSql(saveWordQuery);
+
+      for (let i = 0; i < word.phrases.length; i++) {
+        const saveWordExplanationQuery = `INSERT INTO ${
+          this.wordExamplesTableName
+        }(
+          word_id,
+          phrase,
+        ) VALUES (
+          last_insert_rowid(),
+          '${word.phrases.at(i)}'
+        );`;
+
+        // console.log(saveWordExplanationQuery);
+
+        await this.db.executeSql(saveWordExplanationQuery);
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error(`Erro ao salvar palavra: ${error.message}`);
+      }
+    }
+  }
+}
+
+export {WordsDatasourceImpl};
+export type {IWordsDatasource};
--- src/features/home/presentation/pages/home_page.tsx
+++ src/features/home/presentation/pages/home_page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {View} from 'react-native';
 
 import HomePageStyle from './home_page.styles';
@@ -7,34 +7,81 @@ import SearchComponent from '../components/search/search_component';
 import WordsComponent from '../components/words/words_component';
 import WordEntity from '../../domain/entities/word';
 import StatusSelectorComponent from '../components/status_selector/status_selector_component';
-
-const elements: WordEntity[] = [
-  {
-    id: 1,
-    status: 'aprendido',
-    word: 'Red',
-    explanation: 'Red is a color',
-    phrases: ['The car is red'],
-  },
-  {
-    id: 2,
-    status: 'revisar',
-    word: 'Duck',
-    explanation: 'Ducks are birds/waterfowls',
-    phrases: [
-      'The ducks started quacking loudly when we threw them some bread',
-    ],
-  },
-];
+import {getDBConnection} from '../controllers/home_page_controller';
+import {
+  IWordsDatasource,
+  WordsDatasourceImpl,
+} from '../../data/datasources/words_datasource';
+import {SQLiteDatabase} from 'react-native-sqlite-storage';
 
 const HomePage: React.FC = () => {
+  const [status, setStatus] = useState<string>('todos');
+  const [words, setWords] = useState<WordEntity[]>([]);
+
+  const loadWords = useCallback(async (status: string) => {
+    try {
+      const db: SQLiteDatabase = await getDBConnection();
+      const datasource: IWordsDatasource = new WordsDatasourceImpl(db);
+
+      await datasource.init();
+
+      console.log(status);
+
+      if (status === 'todos') {
+        setWords(await datasource.getWords());
+        return;
+      } else {
+        const words: WordEntity[] = await datasource.getWords();
+        let wordsFiltered: WordEntity[] = [];
+
+        for (let i = 0; i < words.length; i++) {
+          const word: WordEntity | undefined = words.at(i);
+
+          if (word?.status === status) {
+            wordsFiltered.push(word);
+          }
+        }
+
+        setWords(wordsFiltered);
+      }
+
+      // await datasource.saveWord({
+      //   id: 1,
+      //   status: 'aprendido',
+      //   word: 'Red',
+      //   explanation: 'Red is a color',
+      //   phrases: ['The car is red'],
+      // });
+
+      // await datasource.saveWord({
+      //   id: 2,
+      //   status: 'revisar',
+      //   word: 'Duck',
+      //   explanation: 'Ducks are birds/waterfowls',
+      //   phrases: [
+      //     'The ducks started quacking loudly when we threw them some bread',
+      //   ],
+      // });
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   loadWords(status);
+  // }, [loadWords]);
+
+  useEffect(() => {
+    loadWords(status);
+  }, [status]);
+
   return (
     <View style={HomePageStyle.container}>
       <SearchComponent />
       <View style={{height: 10}} />
-      <StatusSelectorComponent onPress={status => console.log(status)} />
+      <StatusSelectorComponent onPress={status => setStatus(status)} />
       <View style={{height: 50}} />
-      <WordsComponent words={elements} />
+      <WordsComponent words={words} />
     </View>
   );
 };
